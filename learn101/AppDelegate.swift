3da30f382f3c31bbb54d026597edff89fb2272fb
jeanepaul <jeanepaul@gmail.com>
2015-05-09 01:30:00 +0900

    Initial Commit

---
+++ learn101/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  learn101
+//
+//  Created by ジエーンポール ソリバ on 5/9/15.
+//  Copyright (c) 2015 ジエーンポール ソリバ. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ learn101/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  learn101
+//
+//  Created by ジエーンポール ソリバ on 5/9/15.
+//  Copyright (c) 2015 ジエーンポール ソリバ. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
